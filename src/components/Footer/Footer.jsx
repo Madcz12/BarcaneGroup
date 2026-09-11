81050ee06a15151b585eb953cf2c0d8992d137f1
Madcz12 <diamondmiguel12@gmail.com>
2026-09-11 19:50:44 -0400

    fix: info in Footer component updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -42,10 +42,10 @@ export default function Footer() {
       <div className="container footer-grid">
         {/* Logo and About Column */}
         <div className="footer-col-brand">
-          <Link 
-            to="/" 
+          <Link
+            to="/"
             className="footer-logo"
-            onClick={(e) => handleSectionClick(e, 'inicio')}
+            onClick={(e) => handleSectionClick(e, "inicio")}
           >
             <img
               src="/images/BarcaneGroupLogo-bgless.png"
@@ -130,12 +130,18 @@ export default function Footer() {
           <h3 className="footer-title">Enlaces</h3>
           <ul className="footer-links">
             <li>
-              <a href="#que-hacemos" onClick={(e) => handleSectionClick(e, 'que-hacemos')}>
+              <a
+                href="#que-hacemos"
+                onClick={(e) => handleSectionClick(e, "que-hacemos")}
+              >
                 Nosotros
               </a>
             </li>
             <li>
-              <a href="#que-hacemos" onClick={(e) => handleSectionClick(e, 'que-hacemos')}>
+              <a
+                href="#que-hacemos"
+                onClick={(e) => handleSectionClick(e, "que-hacemos")}
+              >
                 Soluciones
               </a>
             </li>
@@ -143,12 +149,18 @@ export default function Footer() {
               <Link to="/productos">Productos</Link>
             </li>
             <li>
-              <a href="#industrias" onClick={(e) => handleSectionClick(e, 'industrias')}>
+              <a
+                href="#industrias"
+                onClick={(e) => handleSectionClick(e, "industrias")}
+              >
                 Industrias
               </a>
             </li>
             <li>
-              <a href="#contacto" onClick={(e) => handleSectionClick(e, 'contacto')}>
+              <a
+                href="#contacto"
+                onClick={(e) => handleSectionClick(e, "contacto")}
+              >
                 Contacto
               </a>
             </li>
@@ -199,7 +211,7 @@ export default function Footer() {
               >
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
-              <span>943 703 905</span>
+              <span>992441610</span>
             </li>
             <li>
               <svg
